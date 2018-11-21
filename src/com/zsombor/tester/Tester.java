f6bad12b9de30874d83b8ed87100c5084938f929
zsombor <apathy.zsombor@gmail.com>
2018-11-21 10:44:54 +0100

    tester

---
+++ src/com/zsombor/tester/Tester.java
@@ -0,0 +1,14 @@
+package com.zsombor.tester;
+import com.codecool.termlib.*;
+
+
+public class Tester {
+	public static void main(String[] args) {
+        Terminal term = new Terminal();
+        System.out.println("asdfasdfasdfasd");
+        term.clearScreen();
+        System.out.println("asdfasdfasdfasd");
+		
+	
+	}
+}
